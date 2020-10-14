713a2162258db13c1e02daec5d8e0872c8653747
Alexei Pogorelov <alexeipogorelovbn@yandex.ru>
2020-10-14 12:25:43 +0300

    feat: add basic page layout towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
-// You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+	
+	if(!matrix||matrix.length==0) return [];
+
+	matrix.forEach((item,index)=>{
+		if (index%2!==0){item.reverse()}
+	})
+	
+	let newArr = matrix.join(',').split(',');
+	let result=[];
+	
+	  for(let i=0;i<newArr.length;i++){
+		  result.push(+newArr[i]);
+	  }
+	   return result;
+	}
